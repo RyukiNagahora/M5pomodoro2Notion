799bdfa95c5f9cde537095004eea6eacd5a54c30
RyukiNagahora <nagahora@bassdrum.org>
2025-11-05 10:56:43 +0900

    Remove examples folder
    - Remove examples/NotionPostTest directory (no longer needed)
    - Update .gitignore to remove examples-related entries

--- examples/NotionPostTest/secrets_template.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// コピーして secrets.h を作成し、以下の値を実環境用に置き換えてください。
-static const char *const WIFI_SSID = "YOUR_WIFI_SSID";
-static const char *const WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
-static const char *const NOTION_TOKEN = "secret_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
-static const char *const NOTION_DATABASE_ID = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
